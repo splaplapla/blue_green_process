a2a784af9c222ffcde54bf1fdaaa6c5256197047
jiikko <n905i.1214@gmail.com>
2022-07-31 06:42:52 +0900

    first commit

---
+++ lib/blue_green_process.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "blue_green_process/version"
+
+module BlueGreenProcess
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/blue_green_process/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BlueGreenProcess
+  VERSION = "0.1.0"
+end
