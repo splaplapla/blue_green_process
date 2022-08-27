5cf990210a7c66da9b8cc6adfd3e20e70878908a
jiikko <n905i.1214@gmail.com>
2022-08-28 06:49:43 +0900

    単一プロセスでの実行を延長する

--- lib/blue_green_process/config.rb
+++ lib/blue_green_process/config.rb
@@ -20,10 +20,14 @@ module BlueGreenProcess
 
     def shared_variables
       @shared_variables ||= []
+      @shared_variables.push(:extend_run_on_this_process)
+      @shared_variables.uniq
     end
 
     def shared_variables=(value)
       @shared_variables = value.map(&:to_s)
+      @shared_variables.push("extend_run_on_this_process")
+      @shared_variables.uniq
     end
   end
 end
--- lib/blue_green_process/shared_variable.rb
+++ lib/blue_green_process/shared_variable.rb
@@ -8,20 +8,34 @@ module BlueGreenProcess
 
     attr_writer :data
 
+    # @return [Hash]
     def data
       @data ||= {}
     end
 
+    # @return [Boolean]
+    def extend_run_on_this_process
+      @data['extend_run_on_this_process'] ||= false
+    end
+
+    # @return [Boolean]
+    def extend_run_on_this_process=(value)
+      @data['extend_run_on_this_process'] = value
+    end
+
+    # @return [Hash]
     def restore(json)
       return if json.nil?
 
       self.data = json.slice(*BlueGreenProcess.config.shared_variables)
     end
 
+    # @return [Hash]
     def dump
       data.slice(*BlueGreenProcess.config.shared_variables)
     end
 
+    # @return [NilClass]
     def reset
       @data = nil
     end
